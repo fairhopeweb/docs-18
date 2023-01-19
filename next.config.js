959f9fd32568a9cf4efa5474804a832c82f0baea
Moshe Simantov <me@moshe.io>
2023-01-19 19:36:12 +0200

    getting started docs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,30 @@
 /* eslint-disable unicorn/prefer-module, @typescript-eslint/no-var-requires */
 
+const { getHighlighter, BUNDLED_LANGUAGES } = require('shiki');
+const grammar = require('./shiki-languages/neuledge.json');
+
+const rehypePrettyCodeOptions = {
+  getHighlighter: (options) =>
+    getHighlighter({
+      ...options,
+      langs: [
+        ...BUNDLED_LANGUAGES,
+        {
+          id: 'neuledge',
+          scopeName: 'source.states',
+          grammar,
+          aliases: ['states', 'neuledge'],
+        },
+      ],
+    }),
+};
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  mdxOptions: {
+    rehypePrettyCodeOptions,
+  },
 });
 
 /** @type {import('next').NextConfig} */
