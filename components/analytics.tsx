8fce87f2238003c37364bf5ab1a861714bba19a8
Moshe Simantov <me@moshe.io>
2023-02-28 13:22:11 +0200

    use native script for analytics

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -1,5 +1,4 @@
 import { SEGMENT_WRITE_KEY } from '@/config';
-import Script from 'next/script';
 
 export const Analytics = () => {
   if (!SEGMENT_WRITE_KEY) {
@@ -7,11 +6,11 @@ export const Analytics = () => {
   }
 
   return (
-    <Script id="segment-analytics" strategy="afterInteractive">
+    <script id="segment-analytics">
       {`!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="${SEGMENT_WRITE_KEY}";;analytics.SNIPPET_VERSION="4.15.3";
   analytics.load("${SEGMENT_WRITE_KEY}");
   analytics.page();
   }}();`}
-    </Script>
+    </script>
   );
 };
